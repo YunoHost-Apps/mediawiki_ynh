cc124ea33de8a086f29767eb3bd39b0bca9b2437
Félix Piédallu <felix@piedallu.me>
2020-06-11 22:21:24 +0200

    quoting

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,4 +3,9 @@
 #=================================================
 # CONSTANTS
 #=================================================
-pkg_dependencies="diffutils imagemagick acl"
+# shellcheck disable=SC2034 # Variable is used by other scripts
+pkg_dependencies=(
+    diffutils
+    imagemagick
+    acl
+)
