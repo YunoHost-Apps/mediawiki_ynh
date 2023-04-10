aadaaa7f31bcbc69b7009a79a64d668852e53b30
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-10 16:33:31 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="diffutils imagemagick acl"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
